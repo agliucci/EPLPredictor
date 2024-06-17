677d0e2243e4200eedd8136a5c13f80eb77a692d
Alessandro Liucci <aliucci@umass.edu>
2024-06-17 15:20:15 -0400

    Extracted repeated code into function for usability

--- epl_predict.py
+++ epl_predict.py
@@ -1,4 +1,11 @@
+import pandas as pd
 import joblib
+
+df = pd.read_csv("epl_results.csv") 
+
+find_averages = df.drop(columns=["Date", "Time", "Referee", "HTR"])
+
+print(find_averages["FTHG"].mean())
 
 
 model = joblib.load('epl_decision_tree_model.pkl')
@@ -9,7 +16,7 @@ label_encoder_htr = joblib.load('label_encoder_htr.pkl')
 
 data_to_predict = {
     'HomeTeam': ['Man United'],
-    'AwayTeam': ['Chelsea'],
+    'AwayTeam': ['Man City'],
     'FTHG': [2],
     'FTAG': [1],
     'HTHG': [1],
@@ -26,21 +33,56 @@ data_to_predict = {
     'AR': [0]
 }
 
-data_to_predict['HomeTeam_encoded'] = label_encoder_home.transform(data_to_predict['HomeTeam'])
-data_to_predict['AwayTeam_encoded'] = label_encoder_away.transform(data_to_predict['AwayTeam'])
 
-import pandas as pd
 
-# Create DataFrame from dictionary
+
+
+
 new_data = pd.DataFrame(data_to_predict)
 
-# Select relevant features
+
 X_new = new_data[['HomeTeam_encoded', 'AwayTeam_encoded', 'FTHG', 'FTAG', 'HTHG', 'HTAG', 'HST', 'AST', 'HF', 'AF', 'HC', 'AC', 'HY', 'AY', 'HR', 'AR']]
 
-# Make prediction
+
 predictions = model.predict(X_new)
 
-# Inverse transform predictions to get original labels
+
 predicted_outcome = label_encoder_ftr.inverse_transform(predictions)[0]
 print(f"Predicted Outcome: {predicted_outcome}")
 
+
+def average(home_team, away_team):
+    home = find_averages[(find_averages["HomeTeam"] == away_team) & (find_averages["AwayTeam"] == home_team)]
+    away = find_averages[(find_averages["HomeTeam"] == home_team) & (find_averages["AwayTeam"] == away_team)]
+    cols = ["FTHG", "FTAG", "HTHG", "HTAG", "HS", "AS","HST", "AST", "HF", "AF", "HC", "AC", "HY", "AY", "HR", "AR"]
+    home_avg = home[cols].mean().to_dict()
+    away_avg = away[cols].mean().to_dict()
+
+
+    data_to_predict = {
+        'HomeTeam': [home_team],
+        'AwayTeam': [away_team],
+        'FTHG': [home_avg['FTHG']],
+        'FTAG': [away_avg['FTAG']],
+        'HTHG': [home_avg['HTHG']],
+        'HTAG': [away_avg['HTAG']],
+        'HST': [home_avg['HST']],
+        'AST': [away_avg['AST']],
+        'HF': [home_avg['HF']],
+        'AF': [away_avg['AF']],
+        'HC': [home_avg['HC']],
+        'AC': [away_avg['AC']],
+        'HY': [home_avg['HY']],
+        'AY': [away_avg['AY']],
+        'HR': [home_avg['HR']],
+        'AR': [away_avg['AR']],
+    }
+    return data_to_predict
+
+def encode(home_team, away_team):
+    data_to_predict['HomeTeam_encoded'] = label_encoder_home.transform([home_team])
+    data_to_predict['AwayTeam_encoded'] = label_encoder_away.transform([away_team])
+    return data_to_predict['HomeTeam_encoded'], data_to_predict['AwayTeam_encoded']
+
+print(average("Man United", "Man City"))
+print(encode("Man United", "Man City"))
